b4d7828a393b913c258c3dd96f7282effcb3f6f7
uzimaru0000 <shuji365630@gmail.com>
2022-12-27 02:46:13 +0900

    Add multipart/form-data request handler

--- api/super-chat.ts
+++ api/super-chat.ts
@@ -5,16 +5,66 @@ import { loadGoogleFont } from '../lib/fonts';
 import { SuperChat } from '../components/SuperChat';
 import { BBox, Resvg } from '@resvg/resvg-js';
 import fetch from 'node-fetch';
+import { Form, File } from 'multiparty';
+import { readFile } from 'fs/promises';
 
-const getIcon = async (url: string) => {
+type Props = {
+  icon: string | File;
+  name: string;
+  price: number;
+  message: string;
+};
+
+const getIcon = async (data: string | File) => {
+  if (typeof data === 'string') {
+    return fetchIcon(data);
+  } else {
+    return readIcon(data);
+  }
+};
+
+const fetchIcon = async (url: string) => {
   const res = await fetch(url);
   const blob = await res.clone().blob();
   const buf = await res.clone().buffer();
   return `data:${blob.type};base64,${buf.toString('base64')}`;
 };
 
+const readIcon = async (file: File) => {
+  const buf = await readFile(file.path);
+  return `data:${file.headers['content-type']};base64,${buf.toString(
+    'base64'
+  )}`;
+};
+
+const getProps = async (req: VercelRequest): Promise<Props> => {
+  if (req.method === 'GET') {
+    const { icon, name, price, message } = req.query;
+    return { icon, name, price: Number(price), message } as Props;
+  } else if (req.method === 'POST') {
+    const form = new Form();
+    const data = await new Promise<Props>((resolve, reject) => {
+      form.parse(req, function (err, fields, files) {
+        if (err) {
+          reject({ err });
+        } else {
+          resolve({
+            icon: files['icon']?.[0],
+            name: fields['name']?.[0],
+            price: Number(fields['price']),
+            message: fields['message'],
+          });
+        }
+      });
+    });
+    return data;
+  } else {
+    throw new Error('invalid request');
+  }
+};
+
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const { icon, name, price, message } = req.query;
+  const { icon, name, price, message } = await getProps(req);
 
   const notoSans400 = await loadGoogleFont({
     family: 'Noto Sans JP',
@@ -63,4 +113,10 @@ export default async (req: VercelRequest, res: VercelResponse) => {
     .setHeader('content-type', 'image/png')
     .setHeader('Access-Control-Allow-Origin', '*')
     .send(img);
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
